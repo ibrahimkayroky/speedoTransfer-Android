c5bf6eba9bd87b90ee1cab7034f329511532e91a
karensamuel <113302396+karensamuel@users.noreply.github.com>
2024-08-29 23:31:24 +0300

    signup ui finished edit in components add onclick paramter in onclick button

--- app/src/main/java/com/gradproj/SpeedoTransferApp/features/authentication/Signup.kt
+++ app/src/main/java/com/gradproj/SpeedoTransferApp/features/authentication/Signup.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gradproj.SpeedoTransferApp.R
