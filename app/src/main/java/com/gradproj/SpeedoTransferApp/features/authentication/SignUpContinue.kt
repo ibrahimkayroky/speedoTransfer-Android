24d9671d109816d99eab3202727fdaa3647105ef
karensamuel <113302396+karensamuel@users.noreply.github.com>
2024-08-29 23:30:53 +0300

    signup ui finished edit in components add onclick paramter in onclick button

--- app/src/main/java/com/gradproj/SpeedoTransferApp/features/authentication/SignUpContinue.kt
+++ app/src/main/java/com/gradproj/SpeedoTransferApp/features/authentication/SignUpContinue.kt
@@ -2,21 +2,17 @@ package com.gradproj.SpeedoTransferApp.features.authentication
 
 
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
