397f27b9527f1f62236e2e82315dc2f824a9ea86
karensamuel <113302396+karensamuel@users.noreply.github.com>
2024-08-30 18:56:14 +0300

    sign in pagefinish sign in signupcontinue needs some alterations

--- app/src/main/java/com/gradproj/SpeedoTransferApp/MainActivity.kt
+++ app/src/main/java/com/gradproj/SpeedoTransferApp/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.gradproj.SpeedoTransferApp
 
+
+import SignIn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.gradproj.SpeedoTransferApp.features.authentication.SignupContinue
 import com.gradproj.SpeedoTransferApp.features.authentication.UserAuthentication
-import com.gradproj.SpeedoTransferApp.features.error.Error404
-import com.gradproj.SpeedoTransferApp.features.error.NoInternet
 import com.gradproj.SpeedoTransferApp.ui.components.GradientBackground
-import com.gradproj.SpeedoTransferApp.ui.components.GradientBackground2
 import com.gradproj.SpeedoTransferApp.ui.theme.SpeedoTransferAppTheme
 import com.gradproj.SpeedoTransferApp.ui.theme.white
 
@@ -23,30 +23,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SpeedoTransferAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = white
-                ) {
-                    GradientBackground2{
-                    UserAuthentication(modifier = Modifier.padding(16.dp))
-                    //NoInternet(modifier = Modifier.padding(16.dp))
-                    }
-                }
+
+
+                SignIn(modifier = Modifier.padding(16.dp))
+
+
             }
         }
     }
 }
 
-@Preview(showSystemUi = true, showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun UserAuthenticationPreview()
 {
     UserAuthentication()
-}
-
-@Preview(showSystemUi = true, showBackground = true)
-@Composable
-private fun Error404Preview()
-{
-    NoInternet()
 }
